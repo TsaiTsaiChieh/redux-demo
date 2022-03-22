04b5eae7f80f609b42648d09de60ace48851bd9b
Tsai-Chieh <d0177381@gmail.com>
2022-03-22 17:04:39 +0800

    :sparkles: create Reducers

--- index.js
+++ index.js
@@ -6,3 +6,20 @@ const buyCake = () => {
         info: 'First redux function'
     }
 }
+
+// (previousState, action) => newState
+
+const initialState = {
+    numberOfCake: 10
+}
+
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_CAKE: return {
+            ...state,
+            numberOfCake: state.numberOfCake - 1
+        }
+        default: return state
+    }
+}
