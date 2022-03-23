afa2c75aaf9135e682c0c0cd86291595b46a5403
Tsai-Chieh <d0177381@gmail.com>
2022-03-23 16:28:52 +0800

    :sparkles: Mutile Reducers, ref from https://www.youtube.com/watch?v=apcda524MJA&list=PLC3y8-rFHvwheJHvseC3I0HuYI2f46oAK&index=9

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICE_CREAM = 'BUY_ICE_CREAM'
 
 const buyCake = () => {
     return {
@@ -7,10 +11,16 @@ const buyCake = () => {
     }
 }
 
+const buyIceCream = () => {
+    return {
+        type: BUY_ICE_CREAM,
+    }
+}
 // (previousState, action) => newState
 
 const initialState = {
-    numberOfCake: 10
+    numberOfCake: 10,
+    numberOfIceCream: 20
 }
 
 
@@ -20,6 +30,20 @@ const reducer = (state = initialState, action) => {
             ...state,
             numberOfCake: state.numberOfCake - 1
         }
+        case BUY_ICE_CREAM: return {
+            ...state,
+            numberOfIceCream: state.numberOfIceCream - 1
+        }
         default: return state
     }
 }
+
+const store = createStore(reducer)
+console.log('Initial State', store.getState())
+const unsubscribe = store.subscribe(() => console.log('Update state', store.getState()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
+unsubscribe()
